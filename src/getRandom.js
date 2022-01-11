19dc52d94951a1d394a67f1fd8f3163d86958877
MatL98 <longomaty@gmail.com>
2022-01-11 20:18:01 -0300

    add heroku Procfile

--- src/getRandom.js
+++ src/getRandom.js
@@ -1,18 +1,17 @@
 function randomNumber(quantity) {
-    let nums = []
-    for(let i = 1; i <= quantity; i++){
-        nums[i] = (Math.random() * 1000)
-    }
-    return nums
+  let nums = [];
+  for (let i = 1; i <= quantity; i++) {
+    nums[i] = Math.random() * 1000;
+  }
+  return nums;
 }
 
-
-process.on("message", (message)=>{
-    if(message > 1){
-        let random = randomNumber(message)
-        process.send(random)
-    }else{
-        let random = randomNumber(Math.pow(10, 8))
-        process.send(random)
-    }
-})
+process.on("message", (message) => {
+  if (message > 1) {
+    let random = randomNumber(message);
+    process.send(random);
+  } else {
+    let random = randomNumber(Math.pow(10, 8));
+    process.send(random);
+  }
+});
